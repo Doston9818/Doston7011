1f7682947b0d8444241c5917c2e368c2ef4d182c
Rustambek97 <r.baltayev9997@gmail.com>
2023-03-31 13:15:11 +0500

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from telebot import TeleBot, types
 
-token = "5891560470:AAHRZjdIi1xefovR0vMTFctuAIF8eP4vksI"
-
-mybot = TeleBot(token)
+token1 = "5891560470:AAHRZjdIi1xefovR0vMTFctuAIF8eP4vksI"
+token2 = "6189904407:AAF0hGrwb9uWrozvr-dh0VS2o7L6HRBiUxU"
+mybot = TeleBot(token1)
 
 @mybot.message_handler(commands=['start'])
 def boshlash(message):
@@ -10,15 +10,15 @@ def boshlash(message):
 	buttons.row("dushanba","seshanba","chorshanba")
 	buttons.row("payshanba","juma","shanba")
 	mybot.send_message(message.chat.id,"meni botimga hush kelibsiz pastdan kunni tanlang👇",reply_markup=buttons)
-	
-@mybot.message_handler(content_types=['photo'])
+
+@mybot.message_handler(func=lambda message:message.text=="salom")
 def rasm(message):
 	mybot.delete_message(message.chat.id, message.id)
 
-# @mybot.message_handler(func=lambda message:"salom")
-# def habar(message):
-# 	mybot.edit_message_text(chat_id=message.chat.id,message_id=message.id,text="menga_yozma")
-	
+@mybot.message_handler(func=lambda message:message.text=="hayr")
+def habar(message):
+	matn = mybot.send_message(chat_id=message.chat.id,text="hayr")
+	mybot.edit_message_text(text="menga yozma",chat_id=message.chat.id,message_id=matn.id)
 
 @mybot.message_handler(content_types=['text'])
 def habar(message):
@@ -33,7 +33,7 @@ def habar(message):
 		mybot.send_message(message.chat.id,"dushanbadagi darslar👇",reply_markup=buttons)
 
 
-@mybot.message_handler(content_types=['sticker','video','text'])
+@mybot.message_handler(content_types=['sticker','video'])
 def fayl(message):
 	mybot.copy_message(chat_id=message.chat.id,from_chat_id=message.chat.id,message_id=message.id)
 
@@ -44,8 +44,13 @@ def tugma_bosildi(call):
 	if call.data == "bio":
 		mybot.send_message(call.message.chat.id,"114-xona.\n ass: Baltayev.R")
 	elif call.data == "dastur":
-		file = open("photos/dastur.JPG","rb")
+		file = open("/dastur.JPG","rb")
 		mybot.send_photo(call.message.chat.id,file)
+
+@mybot.message_handler(func=lambda message:message.text="seshanba")
+def send_locatsiya_phone(message):
+	tugmalar = types.ReplyKeyboardMarkup(True)
+
 
 mybot.polling()
 
